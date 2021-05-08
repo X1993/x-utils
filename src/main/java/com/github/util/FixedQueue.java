e06360e188dd712543882969c8016b06d9b1f4e1
jie <275899116@qq.com>
2021-05-08 17:14:43 +0800

    完善定长队列

--- src/main/java/com/github/util/FixedQueue.java
+++ src/main/java/com/github/util/FixedQueue.java
@@ -1,7 +1,7 @@
 package com.github.util;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.function.Predicate;
 
 /**
  * 定长队列
@@ -12,7 +12,7 @@ import java.util.List;
  * @author X1993
  * @date 2021-05-08
  */
-public class FixedQueue<E> {
+public class FixedQueue<E> implements Queue<E> {
 
     private final Object[] data;
 
@@ -25,15 +25,12 @@ public class FixedQueue<E> {
      */
     public FixedQueue(int fixed) {
         if (fixed <= 0){
-            throw new IllegalArgumentException("fixed must > 0");
+            throw new IllegalArgumentException();
         }
         data = new Object[fixed];
     }
 
-    /**
-     * 使用空间大小
-     * @return
-     */
+    @Override
     public int size() {
         return len;
     }
@@ -46,19 +43,12 @@ public class FixedQueue<E> {
         return data.length;
     }
 
-    /**
-     * 是否为空
-     * @return
-     */
+    @Override
     public boolean isEmpty() {
         return size() == 0;
     }
 
-    /**
-     * 是否包含指定元素
-     * @param o
-     * @return
-     */
+    @Override
     public boolean contains(Object o) {
         int h = header;
         for (int i = 0; i < len; i++) {
@@ -67,6 +57,157 @@ public class FixedQueue<E> {
             }
         }
         return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        //偷个懒
+        return list().iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return toArray(new Object[len]);
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        int h = header;
+        for (int i = 0; i < len; i++) {
+            a[i] = (T) data[(h++) % data.length];
+        }
+        return a;
+    }
+
+    /**
+     * 按添加顺序生成List
+     * @return
+     */
+    public List<E> list()
+    {
+        List<E> list = new ArrayList<>(len + 1);
+        int h = header;
+        for (int i = 0; i < len; i++) {
+            list.add((E) data[(h++) % data.length]);
+        }
+        return list;
+    }
+
+    @Override
+    public boolean add(E e)
+    {
+        if (len == data.length){
+            //满了
+            data[header++ % data.length] = e;
+        } else if (len < data.length){
+            data[(header + len++) % data.length] = e;
+        } else {
+            throw new IllegalStateException();
+        }
+        return true;
+    }
+
+    private boolean remove(Predicate<Object> predicate)
+    {
+        int h = header;
+        int removeCount = 0;
+        for (int i = 0; i < len; i++) {
+            int index = (h++) % data.length;
+            if (predicate.test(data[index])) {
+                removeCount++;
+            }else if (removeCount > 0){
+                //前移
+                data[(index - removeCount + data.length) % data.length] = data[index];
+            }
+        }
+        len -= removeCount;
+        return removeCount > 0;
+    }
+
+    @Override
+    public boolean remove(Object o)
+    {
+        return remove(e -> e.equals(o));
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c)
+    {
+        Set<?> set;
+        if (c instanceof Set){
+            set = (Set<?>) c;
+        }else {
+            set = new HashSet<>(c);
+        }
+        return remove(e -> set.contains(e));
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        Set<?> set;
+        if (c instanceof Set){
+            set = (Set<?>) c;
+        }else {
+            set = new HashSet<>(c);
+        }
+        return remove(e -> !set.contains(e));
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return c.stream().allMatch(x -> contains(x));
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        for (E e : c) {
+            add(e);
+        }
+        return true;
+    }
+
+    @Override
+    public void clear() {
+        len = 0;
+    }
+
+    @Override
+    public boolean offer(E e) {
+        return add(e);
+    }
+
+    @Override
+    public E poll() {
+        if (len <= 0){
+            return null;
+        }
+        E e = (E) data[header];
+        header = (header + 1) % data.length;
+        len--;
+        return e;
+    }
+
+    @Override
+    public E remove() {
+        E poll = poll();
+        if (poll == null){
+            throw new NoSuchElementException();
+        }
+        return poll;
+    }
+
+    @Override
+    public E element() {
+        E first = get(0);
+        if (first == null){
+            throw new NoSuchElementException();
+        }
+        return first;
+    }
+
+    @Override
+    public E peek() {
+        return get(0);
     }
 
     /**
@@ -95,51 +236,6 @@ public class FixedQueue<E> {
      */
     public E getFirst(){
         return get(0);
-    }
-
-    /**
-     * 按添加顺序生成List
-     * @return
-     */
-    public List<E> list()
-    {
-        List<E> list = new ArrayList<>(len + 1);
-        int h = header;
-        for (int i = 0; i < len; i++) {
-            list.add((E) data[(h++) % data.length]);
-        }
-        return list;
-    }
-
-    /**
-     * 入队
-     * @param e
-     * @return
-     */
-    public boolean push(E e) {
-        if (len == data.length){
-            //满了
-            data[header++ % data.length] = e;
-        } else if (len < data.length){
-            data[(header + len++) % data.length] = e;
-        } else {
-            throw new IllegalStateException();
-        }
-        return true;
-    }
-
-    /**
-     * 出队
-     * @return
-     */
-    public E poll() {
-        if (len <= 0){
-            return null;
-        }
-        E e = (E) data[header];
-        header = (header + 1) % data.length;
-        len--;
-        return e;
     }
 
 }
--- src/test/java/com/github/util/FixedQueueTest.java
+++ src/test/java/com/github/util/FixedQueueTest.java
@@ -2,6 +2,7 @@ package com.github.util;
 
 import org.junit.Assert;
 import org.junit.Test;
+import java.util.Arrays;
 
 public class FixedQueueTest {
 
@@ -14,8 +15,8 @@ public class FixedQueueTest {
 
         int value = 0;
 
-        fixedQueue.push(value++);
-        fixedQueue.push(value++);
+        fixedQueue.add(value++);
+        fixedQueue.add(value++);
         Assert.assertEquals(fixedQueue.size() ,2);
         Assert.assertFalse(fixedQueue.isEmpty());
 
@@ -24,9 +25,9 @@ public class FixedQueueTest {
         Assert.assertEquals(fixedQueue.get(1).intValue() ,1);
         Assert.assertNull(fixedQueue.get(2));
 
-        fixedQueue.push(value++);
-        fixedQueue.push(value++);
-        fixedQueue.push(value++);
+        fixedQueue.add(value++);
+        fixedQueue.add(value++);
+        fixedQueue.offer(value++);
         Assert.assertEquals(fixedQueue.size() ,3);
         Assert.assertEquals(fixedQueue.get(0).intValue() ,2);
         Assert.assertEquals(fixedQueue.get(1).intValue() ,3);
@@ -42,14 +43,44 @@ public class FixedQueueTest {
         Assert.assertEquals(fixedQueue.get(0).intValue() ,4);
         Assert.assertNull(fixedQueue.get(1));
 
-        fixedQueue.push(value++);
-        fixedQueue.push(value++);
+        fixedQueue.offer(value++);
+        fixedQueue.add(value++);
         Assert.assertEquals(fixedQueue.size() ,3);
 
         v = 4;
         for (Integer integer : fixedQueue.list()) {
             Assert.assertTrue(integer.intValue() == v++);
         }
+
+        fixedQueue.addAll(Arrays.asList(10 ,11 ,12));
+        Assert.assertEquals(fixedQueue.size() ,3);
+
+        Assert.assertTrue(fixedQueue.remove(11));
+        Assert.assertEquals(fixedQueue.size() ,2);
+        Assert.assertTrue(fixedQueue.getFirst().intValue() == 10);
+        Assert.assertTrue(fixedQueue.get(0).intValue() == 10);
+        Assert.assertTrue(fixedQueue.getLast().intValue() == 12);
+        Assert.assertTrue(fixedQueue.get(1).intValue() == 12);
+        Assert.assertNull(fixedQueue.get(2));
+
+        fixedQueue.addAll(Arrays.asList(12 ,11 ,12));
+
+        Assert.assertTrue(fixedQueue.remove(12));
+        Assert.assertEquals(fixedQueue.size() ,1);
+        Assert.assertTrue(fixedQueue.getFirst().intValue() == 11);
+        Assert.assertTrue(fixedQueue.get(0).intValue() == 11);
+        Assert.assertTrue(fixedQueue.getLast().intValue() == 11);
+        Assert.assertNull(fixedQueue.get(1));
+
+
+        fixedQueue.addAll(Arrays.asList(12 ,11 ,12));
+
+        Assert.assertTrue(fixedQueue.removeAll(Arrays.asList(11 ,12)));
+        Assert.assertEquals(fixedQueue.size() ,0);
+        Assert.assertNull(fixedQueue.getFirst());
+        Assert.assertNull(fixedQueue.get(0));
+
+
     }
 
 }
