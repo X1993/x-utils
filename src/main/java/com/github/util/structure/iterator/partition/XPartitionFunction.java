5ac5826a1466285543564836bdb525fe3b625d2d
wangjj7 <wangjj7@wedoctor.com>
2023-04-27 14:52:09 +0800

    完善注释

--- src/main/java/com/github/util/structure/iterator/partition/XPartitionFunction.java
+++ src/main/java/com/github/util/structure/iterator/partition/XPartitionFunction.java
@@ -10,6 +10,10 @@ import java.util.List;
 @FunctionalInterface
 public interface XPartitionFunction<T> {
 
+    /**
+     * @param prePartitionList 上一个分区查询结果
+     * @return 下一个分区查询结果
+     */
     List<T> select(List<T> prePartitionList);
 
 }
